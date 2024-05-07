c36a3f453efdd5f8709351dbb57ecd4e1033cd66
ziatzkia <zia.tazkiatul@mhs.itenas.ac.id>
2024-05-07 10:53:33 +0700

    add new header

--- index.php
+++ index.php
@@ -8,9 +8,20 @@ include('layouts/header.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Createlier</title>
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <!-- Navbar -->
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Createlier</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-lv7jmEf3DPBjW5zGPrmSdAx2vIEcJyTd1QzStcF4nY8fcw8Ezg6F0MFE0JmwOqFQ" crossorigin="anonymous"></script>
 </body>
 </html>
